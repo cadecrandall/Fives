9cd79033c36b353e793c89e257524a695a65de52
cadecrandall <ccrandall@mymail.mines.edu>
2020-08-07 22:37:56 -0500

    Refactor

--- Functions.cpp
+++ Functions.cpp
@@ -3,6 +3,10 @@
 //
 
 #include "Functions.h"
+#include "Gameboard.h"
+#include <iostream>
+
+using namespace std;
 
 int determineBestScore(std::vector<int> scores) {
     int bestIdx = 0;
@@ -10,4 +14,28 @@ int determineBestScore(std::vector<int> scores) {
         bestIdx = (scores.at(i) > scores.at(bestIdx) ? i : bestIdx);
     }
     return bestIdx;
+}
+
+void makePlayerMove(Gameboard &gb, char move) {
+    switch (move) {
+        case 'W':
+        case 'w':
+            gb.moveUp();
+            break;
+        case 'A':
+        case 'a':
+            gb.moveLeft();
+            break;
+        case 'S':
+        case 's':
+            gb.moveDown();
+            break;
+        case 'D':
+        case 'd':
+            gb.moveRight();
+            break;
+        default:
+            cerr << "Invalid move. Use WASD to move the board" << endl;
+            return;
+    } gb.newTile();
 }
--- Functions.h
+++ Functions.h
@@ -5,7 +5,12 @@
 #ifndef SFML_TEMPLATE_FUNCTIONS_H
 #define SFML_TEMPLATE_FUNCTIONS_H
 #include <vector>
+#include <iostream>
+#include "Gameboard.h"
+
+using namespace std;
 
 int determineBestScore(std::vector<int> scores);        // returns the index of the best score in the leaderboard
+void makePlayerMove(Gameboard &gb, char move);
 
 #endif //SFML_TEMPLATE_FUNCTIONS_H
